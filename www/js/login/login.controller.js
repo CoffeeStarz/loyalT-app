46659902cc7453a511a088d163df9691d5d76586
Octowl <alsaff1987@gmail.com>
2016-06-18 17:46:21 -0400

    Scrambled eggs like my brain is how this app came together finally. sort of

--- www/js/login/login.controller.js
+++
@@ -1,9 +0,0 @@
-// coffeeCard.controller('LoginCtrl', function ($scope, $state, AuthFactory) {
-
-//     $scope.login = {};
-//     $scope.error = null;
-
-//     $scope.sendLogin = AuthFactory.sendLogin;
-    
-
-// });
--- www/js/login/login.state.js
+++
@@ -1,9 +0,0 @@
-// coffeeCard.config(function ($stateProvider) {
-
-//     $stateProvider.state('login', {
-//         url: '/login',
-//         templateUrl: '/js/login/login.html',
-//         controller: 'LoginCtrl'
-//     });
-
-// });
