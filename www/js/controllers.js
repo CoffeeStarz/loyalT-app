1ae04c97bf0cfb9a78bc5dbe7cf9d9803bc9e69e
Reed Branson <reed.branson@gmail.com>
2016-11-17 12:13:22 -0500

    add and remove drinks, grey outline on stars, new placeholder for new users

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,10 +3,10 @@ var coffeeCard = angular.module('coffeeCard.controllers', [])
 .controller('PhoneCtrl', function ($scope, $state, $ionicModal, CardFactory, rewards, $log, $timeout) {
 
   //Card functionnality
-  $scope.submit = function (phoneNumber) {
+  $scope.submit = function (phoneNumber, numOrders) {
     CardFactory.findOrCreate(phoneNumber)
       .then(function (card) {
-        $timeout(function(){card.updateDrinks(1)}, 500)
+        $timeout(function(){card.updateDrinks(numOrders)}, 500)
         $scope.card = card;
         $scope.cardModal.show();
       })
@@ -21,6 +21,22 @@ var coffeeCard = angular.module('coffeeCard.controllers', [])
         };
       });
   };
+
+  $scope.numOrders = 1;
+
+  $scope.addDrink = function () {
+    $scope.numOrders ++;
+    console.log('adding a drink, now have: ', $scope.numOrders , " orders")
+  }
+
+  $scope.removeDrink = function () {
+    if ($scope.numOrders > 1) {
+      $scope.numOrders --;
+    } else {
+      $scope.numOrders = 1;
+    }
+    console.log('removing a drink, now have: ', $scope.numOrders , " orders")
+  }
 
   $scope.rewards = rewards;
 
@@ -39,6 +55,7 @@ var coffeeCard = angular.module('coffeeCard.controllers', [])
 
   $scope.$on('modal.hidden', function() {
     $scope.phoneNumber = null;
+    $scope.numOrders = 1;
   });
 
 });
